cae18207ff946e8509894fcca793db60e90944b6
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-12-03 17:43:42 +0000

    beinagrind fyrir CustomerService Klasann

---
+++ services/customerservice.py
@@ -0,0 +1,24 @@
+class CustomerService(object):
+    def __init__(self):
+        pass
+
+    def make_customer(self, name, SSN, creditcard_number, phone_number):
+        # veit ekki alveg hvernig ég geri þetta, set bara uppkast hér
+        #customer = CustomerRepo.add_customer(name, SSN, creditcard_number, phone_number)
+        pass
+
+    def remove_customer(self, name):
+        # svipaðan hátt og hér að ofan
+        pass
+
+    def change_card(self, name, new_credit):
+        pass
+
+    def change_info(self, info):
+        # Hér þarf svo sannarlega að breyta ýmsu
+
+    def show_list(self):
+        pass
+
+    def show_orders():
+        pass
