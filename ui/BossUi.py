4140cf1c686ad9443d86baf02c8b82f74fd3a683
theodorkri <theodorkri@ru.is>
2018-12-04 17:32:06 +0000

    Basically allt BossUI harðkóðað

--- ui/BossUi.py
+++ ui/BossUi.py
@@ -1,7 +1,8 @@
 #Teddi sér um þennan fæl
 from datetime import date
-HOMECOMMANDS = ["h", "H", "s", "S"]
-
+HOMECOMMANDS = ["h", "s"]
+POSSIBLE_ACTIONS = "\t1. Pantanir\n\t2. Bílayfirlit\n\
+\t3. Viðskiptavinir\n\t4. Starfsmenn\n\t5. Verðlisti\n\t6. Tekjur\n"
 
 class BossUI(object):
     #Sér um viðmót yfirmanns
@@ -25,12 +26,10 @@ class BossUI(object):
     def main_menu(self):
         #Sýnir upphafsviðmót yfirmanns
         choice = ""
-        while choice != HOMECOMMANDS[2] and choice != HOMECOMMANDS[3]:
-            choice = self.show_menu(
-                """ \t1. Pantanir\n\t2. Bílayfirlit
-\t3. Viðskiptavinir\n\t4. Starfsmenn\n\t5. Verðlisti\n\t6. Tekjur\n""")
+        while choice.lower() != HOMECOMMANDS[1]:
+            choice = self.show_menu(POSSIBLE_ACTIONS)
             if choice == "1":
-                choice = self.orders()
+                choice = self.show_all_orders()
             elif choice == "2":
                 choice = self.car_menu()
             elif choice == "3":
@@ -38,20 +37,18 @@ class BossUI(object):
             elif choice == "4":
                 self.show_employees()
             elif choice == "5":
-                pass
-                # ég hef ekki hugmynd um hvernig við ætlum að sýna verðlistann
+                self.price_menu()
             elif choice == "6":
-                pass
+                self.revenue()
 
-    def orders(self):
+    def show_all_orders(self):
         #from services import orderservice
+        self.print_header()
         print("\tdagsetning  |  Pönt.nr.  |  Nafn  |  Kennitala  |  Tegund  |  Bílnr.  |  Staða\n"+("-")*100)
         choice = ""
         while choice not in HOMECOMMANDS:
             choice = input("")
         return choice
-        
-
         
     def car_menu(self):
         #Sýnir bílayfirlitsviðmót yfirmanns og kallar á klasa eftir því sem við á
@@ -71,14 +68,44 @@ class BossUI(object):
 
     def show_customers(self):
         """Prints all customers for boss"""
+        self.print_header()
         print("\tKennitala  |  Nafn  |  Sími\n"+("-")*100)
+        choice = ""
+        while choice not in HOMECOMMANDS:
+            choice = input("")
+        return choice
     
     def show_employees(self):
+        self.print_header()
         print("\tNotendanafn  |  Hlutverk  |  Nafn  |  Sími  |  Heimilisfang\n"+("-")*100)
+        choice = ""
+        while choice not in HOMECOMMANDS:
+            choice = input("")
+        return choice
 
     def price_menu(self):
         #Sýnir verðlistaviðmót yfirmanns og kallar á klasa eftir því sem við á
-        pass
+        self.print_header()
+        print("Verðlisti\n\t{:<12} | {:<12}".format("Jeppi","10000/dag"))
+        print("\t{:<12} | {:<12}".format("Fólksbíll","500/dag"))
+        print("\t{:<12} | {:<12}".format("Sendibíll","7000/dag"))
+        print("\t{:<12} | {:<12}".format("Aukatrygging","5000/dag"))
+        choice = ""
+        while choice not in HOMECOMMANDS:
+            choice = input("\nBreyta verði (F)ólksbíll, (J)eppi, (S)endibíll, (A)uka trygging: ")
+        return choice
+    
+    def revenue(self):
+        self.print_header()
+        print("Tekjur\n\t{:<25} | {:<10}\n\t".format("Pöntunarnúmer","Tekjur")+("-")*38)
+        print("\t{:<25} | {:>10}".format("000001", "120.000 kr"))
+        print("\t{:<25} | {:>10}".format("000002", "10.000 kr"))
+        print("\n\t{:<25} | {:<10}\n\t".format("Mánuður","Tekjur")+("-")*38)
+        print("\t{:<25} | {:>10}".format("11", "130.000 kr"))
+        choice = ""
+        while choice not in HOMECOMMANDS:
+            choice = input("")
+        return choice
 
     def quit(self):
         #Fer á upphafsskjá
