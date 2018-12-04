dce73244e255b0a6420205b6a6875c04f0314f87
Steingrímur Eyjólfsson <steingrimure@gmail.com>
2018-12-04 18:17:58 +0000

    Fiktadi i mainfallinu minu og profadi nokkra hluti i customer repo

---
+++ denni.py
@@ -0,0 +1,5 @@
+from repositories.customerrepo import CustomerRepo
+
+
+k1 = CustomerRepo()
+k1.add_customer("191919191,Joi,282828282,191919199191919")
--- repositories/customerrepo.py
+++ repositories/customerrepo.py
@@ -6,8 +6,14 @@ class CustomerRepo(object):
     def __init__(self):
         self.__customer = {}
 
-    def add_customer(self):
-        pass
+    def add_customer(self, customer):
+        with open("./data/customers.csv", "a+") as customer_file:
+            ssn = customer.get__ssn()
+            name = customer.get_name()
+            phone_number = customer.get_phone_number()
+            creditcard_number = customer.get_creditcard_number()
+            customer_file.write("{},{},{},{}".format(
+                ssn, name, phone_number, creditcard_number))
 
     def remove_customer(self):
         pass
