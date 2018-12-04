540023f03d87f4aeb9b18924d2e0d190dcdb0a36
Aunnsteinsson <atliunnsteinsson@gmail.com>
2018-12-04 14:56:44 +0000

    línur lagaðar til að þær séu ekki lengri en 79

--- models/employee.py
+++ models/employee.py
@@ -1,16 +1,18 @@
 class Employee(object):
-    """Þessi klasi er með nokkrar breytur, (phonenumber og address eru default því það er ekki bráðnauðsynlegar upplýsingar) og heimilar það að þær breytur
-    séu sóttar og þeim breytt með föllum"""
+    """þessi klasi er með nokkrar breytur, (phonenumber og address eru default 
+    því það er ekki bráðnauðsynlegar upplýsingar) og heimilar það að þær 
+    breytur séu sóttar og þeim breytt með föllum"""
 
-    def __init__(self, username, password, type, name, phonenumber="N/A", address="N/A"):
+    def __init__(self, username, password,
+                 type, name, phonenumber="N/A", address="N/A"):
         self.__username = username
         self.__name = name
         self.__password = password
         self.__type = type
         self.__phone_number = phonenumber
         self.__address = address
-    # Hér eru nokkur get föll sett inn til að nálgast breyturnar í fallinu
 
+    # hér eru nokkur get föll sett inn til að nálgast breyturnar í fallinu
     def get_name(self):
         return self.__name
 
@@ -28,8 +30,8 @@ class Employee(object):
 
     def get_phone_number(self):
         return self.__phone_number
-    # Hér eru nokkur change föll til að breyta breytunum.
 
+    # hér eru nokkur change föll til að breyta breytunum.
     def change_name(self, new_name):
         self.__name = new_name
 
@@ -49,9 +51,15 @@ class Employee(object):
         self.__phone_number = new_phone_number
 
     def __str__(self):
-        """Her ery prentaðar allar upplýsingar nema password því það eiga ekki allir að hafa aðgang að passwordi"""
-        return "Nafn: {}, Notendanafn: {}, Staða: {}, Símanúmer: {}, Heimilisfang: {}".format(self.__name, self.__username, self.__type, self.__phone_number, self.__address)
+        """Her ery prentaðar allar upplýsingar nema password
+         því það eiga ekki allir að hafa aðgang að passwordi"""
+        return "Nafn: {}, Notendanafn: {}, Staða: {}, Símanúmer: {}, \
+        Heimilisfang: {}".format(self.__name, self.__username, self.__type,
+                                 self.__phone_number, self.__address)
 
     def __repr__(self):
-        """Hér eru allar upplýsingar prentaðar með kommu á milli. Þetta er gert fyrir admin"""
-        return "{},{},{},{},{},{}".format(self.__name, self.__username, self.__password, self.__type, self.__phone_number, self.__address)
+        """Hér eru allar upplýsingar prentaðar
+         með kommu á milli. Þetta er gert fyrir admin"""
+        return "{},{},{},{},{},{}".format(self.__name, self.__username,
+                                          self.__password, self.__type,
+                                          self.__phone_number, self.__address)
