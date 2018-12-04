e4786a1bc41247beed5d76a5dbebbb198cb411b5
theodorkri <theodorkri@ru.is>
2018-12-04 13:56:04 +0000

    Sett upp beinagrind fyrir Tedda fæla

---
+++ ui/BossUi.py
@@ -0,0 +1,26 @@
+#Teddi sér um þennan fæl
+
+class BossUI(object):
+    #Sér um viðmót yfirmanns
+    def __init__(self, username):
+        self.username = username #strengur sem inniheldur notendanafn
+    
+    def main_menu(self):
+        #Sýnir upphafsviðmót yfirmanns
+        pass
+
+    def car_menu(self):
+        #Sýnir bílayfirlitsviðmót yfirmanns og kallar á klasa eftir því sem við á
+        pass
+
+    def price_menu(self):
+        #Sýnir verðlistaviðmót yfirmanns og kallar á klasa eftir því sem við á
+        pass
+
+    def quit(self):
+        #Fer á upphafsskjá
+        pass
+
+
+    
+    
