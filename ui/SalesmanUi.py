16779de0dcc5f5dd8d0487678c1593421a6dc596
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-12-04 12:04:59 +0000

    SalesmanUI lagaður og bætt við menus

--- ui/SalesmanUi.py
+++ ui/SalesmanUi.py
@@ -13,34 +13,52 @@ class SalesmanUI(object):
             "Söludeild - notandi: {}".format(self.__name), str(date.today()), right_justified))
         print(("-"*80))
 
-    def show_menu(self):
+    def show_menu(self, texti):
         self.print_header()
-        print("""1. Pantanir
-        2. Fá yfirlit yfir alla viðskiptavini
-        3. Viðskiptavinir
-        4. Verðlisti\n""")
+        print(texti)
 
-        choice = input("Veldur síðu: ")
+        choice = input("Veldu síðu: ")
         # Inn í þetta vantar að prenta út það sem er fyrir neðan
         return choice
 
     def main_menu(self):
         # While skipunin er fkd. Maður festist í henni
         choice = ""
-        while choice.lower != "s":
-            choice = self.show_menu()
-            # Hér vantar svo tengingar í hin viðmótin og að prenta verðlista
+        while choice.lower() != "s":
+            choice = self.show_menu(
+                "\t1. Pantanir\n\t2. Bílayfirlit\n\t3. Viðskiptavinir\n\t4. Verðlisti\n")
+            if choice == "1":
+                choice = self.order_menu()
+            elif choice == "2":
+                choice = self.car_menu()
+            elif choice == "3":
+                self.customer_menu()
+            elif choice == "4":
+                pass
+                # ég hef ekki hugmynd um hvernig við ætlum að sýna verðlistann
 
-    def car_menu(self):
-        """Prints car menu and follows up on commands"""
-        pass
+    def order_menu(self):
+        """Prints order menu and follows up on commands"""
+        while 1 > 0:
+            choice = self.show_menu(
+                "Pantanir\n\n1. Yfirlit pantana\n2. Ný pöntun")
+            if choice == "1":
+                # CustomerService.show_list()
+                pass
+            elif choice == "2":
+                # CustomerService.make_customer()
+                pass
+            elif choice == "h":
+                return choice
+            elif choice == "s":
+                return choice
 
     def customer_menu(self):
         """Prints customer menu and follows up on commands"""
         pass
 
-    def order_menu(self):
-        """Prints order menu and follows up on commands"""
+    def car_menu(self):
+        """Prints car menu and follows up on commands"""
 
     def quit(self):
         # Hér þarf að ákveða hvort þetta sé við að hætta í login fallinu
