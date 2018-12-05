e036e6701acdc15029a52a84bef0d1af1756cb3c
Aunnsteinsson <atliunnsteinsson@gmail.com>
2018-12-05 14:44:42 +0000

    remove employee komid i employeerepo

--- repositories/employeerepo.py
+++ repositories/employeerepo.py
@@ -21,5 +21,18 @@ class EmployeeRepo(object):
     def change_info(self, choice, new_value):
         pass
 
-    def remove_employee(self):
-        pass
+    def remove_employee(self, username):
+        with open("./data/employees.csv", "r") as employees_input:
+            with open("./data/employees_edit.csv", "w") as employees_output:
+                csv_reader = csv.reader(employees_input)
+                csv_writer = csv.writer(employees_output)
+                for row in csv_reader:
+                    if row[0] != username:
+                        csv_writer.writerow(row)
+
+        with open("./data/employees.csv", "w") as new_employees_file:
+            with open("./data/employees_edit.csv", "r") as new_employees_edit:
+                csv_reader = csv.reader(new_employees_edit)
+                csv_writer = csv.writer(new_employees_file)
+                for row in csv_reader:
+                    csv_writer.writerow(row)
