4f535372a88a3fd78a5a90d4211d8783e7b00bba
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-12-05 12:50:23 +0000

    customer service bætt

--- services/customerservice.py
+++ services/customerservice.py
@@ -3,25 +3,25 @@ from repositories.customerrepo import CustomerRepo
 
 class CustomerService(object):
     def __init__(self):
-        self.__customer_repo = CustomerRepo()
+        self.__customer_repo = CustomerRepo
 
     def make_customer(self, customer):
         self.__customer_repo.add_customer(customer)
 
     def remove_customer(self, ssn):
-        CustomerRepo.remove_customer(ssn)
+        self.__customer_repo.remove_customer(ssn)
 
     def change_card(self, ssn, new_credit):
-        CustomerRepo.change_card_number(ssn, new_credit)
+        self.__customer_repo.change_card_number(ssn, new_credit)
 
     def change_phone_number(self, ssn, new_phone_number):
-        CustomerRepo.change_card_number(ssn, new_phone_number)
+        self.__customer_repo.change_card_number(ssn, new_phone_number)
 
     def change_name(self, ssn, new_name):
-        CustomerRepo.change_name(ssn, new_name)
+        self.__customer_repo.change_name(ssn, new_name)
 
     def show_list(self):
-        CustomerRepo.get_customers()
+        self.__customer_repo.get_customers()
 
     def show_orders():
         pass
