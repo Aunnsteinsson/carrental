404b01692f5562742ae9e4e512047c3be8655167
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-12-05 17:22:01 +0000

    hægt að breyta info í customer

--- services/customerservice.py
+++ services/customerservice.py
@@ -22,13 +22,25 @@ class CustomerService(object):
         self.__customer_repo.remove_customer(ssn)
 
     def change_card(self, ssn, new_credit):
-        self.__customer_repo.change_card_number(ssn, new_credit)
+        customer_list = self.__customer_repo.get_customer(ssn)
+        customer_class = Customer(
+            customer_list[0], customer_list[1], customer_list[2], new_credit)
+        self.remove_customer(ssn)
+        self.make_customer(customer_class)
 
     def change_phone_number(self, ssn, new_phone_number):
-        self.__customer_repo.change_card_number(ssn, new_phone_number)
+        customer_list = self.__customer_repo.get_customer(ssn)
+        customer_class = Customer(
+            customer_list[0], customer_list[1], new_phone_number, customer_list[3])
+        self.remove_customer(ssn)
+        self.make_customer(customer_class)
 
     def change_name(self, ssn, new_name):
-        self.__customer_repo.change_name(ssn, new_name)
+        customer_list = self.__customer_repo.get_customer(ssn)
+        customer_class = Customer(
+            customer_list[0], new_name, customer_list[2], customer_list[3])
+        self.remove_customer(ssn)
+        self.make_customer(customer_class)
 
     def get_list(self):
         list = self.__customer_repo.get_customers()
