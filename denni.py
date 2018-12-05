c01bde769b616ff5072edec79cfd9c591817cfff
Steingrímur Eyjólfsson <steingrimure@gmail.com>
2018-12-05 13:47:21 +0000

    Eyda customer fallid i customer repo var baett

--- denni.py
+++ denni.py
@@ -1,5 +1,14 @@
-from repositories.customerrepo import CustomerRepo
+import csv
 
 
-k1 = CustomerRepo()
-k1.add_customer("191919191,Joi,282828282,191919199191919")
+def bla(x):
+    with open("./data/customers.csv", "r") as customer_input:
+        with open("./data/customers_edit.csv", "w") as customer_output:
+            csv_reader = csv.reader(customer_input)
+            csv_writer = csv.writer(customer_output)
+            for row in csv_reader:
+                if row[0] != x:
+                    csv_writer.writerow(row)
+
+
+bla("1234567890")
--- repositories/customerrepo.py
+++ repositories/customerrepo.py
@@ -15,8 +15,21 @@ class CustomerRepo(object):
             customer_file.write("\n{},{},{},{}".format(
                 ssn, name, phone_number, creditcard_number))
 
-    def remove_customer(self):
-        pass
+    def remove_customer(self, ssn):
+        with open("./data/customers.csv", "r") as customer_input:
+            with open("./data/customers_edit.csv", "w") as customer_output:
+                csv_reader = csv.reader(customer_input)
+                csv_writer = csv.writer(customer_output)
+                for row in csv_reader:
+                    if row[0] != ssn:
+                        csv_writer.writerow(row)
+
+        with open("./data/customers.csv", "w") as new_customer_file:
+            with open("./data/customers_edit.csv", "r") as new_customer_edit:
+                csv_reader = csv.reader(new_customer_edit)
+                csv_writer = csv.writer(new_customer_file)
+                for row in csv_reader:
+                    csv_writer.writerow(row)
 
     def get_customer(self):
         pass
