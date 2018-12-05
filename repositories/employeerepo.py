436c161882be787ed56800107a5a75fb50d37e84
Aunnsteinsson <atliunnsteinsson@gmail.com>
2018-12-05 12:43:56 +0000

    employeerepo og service tengdir saman til að geta bætt við nýjum employee í employees.csv

--- repositories/employeerepo.py
+++ repositories/employeerepo.py
@@ -1,12 +1,24 @@
+from models.employee import Employee
+import csv
+
+
 class EmployeeRepo(object):
     def __init__(self):
-        pass
+        self.__employee = {}
 
     def get_employee(self):
         pass
 
-    def add_employee(self):
-        pass
+    def add_employee(self, employee):
+        with open("./data/employees.csv", "a+") as employees_file:
+            username = employee.get__username()
+            password = employee.get__password()
+            position = employee.get__type()
+            name = employee.get__name()
+            phone = employee.get__phone_number()
+            address = employee.get_address()
+            employees_file.write("\n{},{},{},{},{},{}".format(
+                username, password, position, name, phone, address))
 
     def change_info(self, choice, new_value):
         pass
