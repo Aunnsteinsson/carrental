73e76e8f3c263958ba0f028716e31b71fa8e0405
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-12-06 12:04:00 +0000

    Breyti salesman

--- services/carservice.py
+++ services/carservice.py
@@ -1,45 +1,31 @@
-#Teddi á að massa þennan fæl
+# Teddi á að massa þennan fæl
 from repositories.carrepo import CarRepo
 from models.car import Car
 
+
 class CarService(object):
-    #Sér um aðgerðir með bíla
+    # Sér um aðgerðir með bíla
     def __init__(self):
         self.__car_repo = CarRepo()
 
     def make_car(self, car):
-        #Nýskráir bíl í kerfi
+        # Nýskráir bíl í kerfi
         self.__car_repo.add_car(car)
-    
+
     def remove_car(self, licence_plate):
-        #Fjarlægir bíl úr kerfi
+        # Fjarlægir bíl úr kerfi
         self.__car_repo.remove_car(licence_plate)
-    
+
     def change_status(self, licence_plate, new_status):
-        #Breytir stöðu bíls
+        # Breytir stöðu bíls
         self.__car_repo.change_status(licence_plate, new_status)
 
     def show_cars(self, licence_plate):
-        #Fall sem sýnir lista af bílum
+        # Fall sem sýnir lista af bílum
         self.__car_repo.get_car(licence_plate)
 
-    def get_all_cars(self):
-        #Heavy copyright Denni
-        list = self.__car_repo.get_all_cars()
-        car_list = []
-        for car in list:
-            if len(car) == 3:
-                car_class = Car(
-                    car[0], car[1], car[2])
-                car_list.append(car_class)
-        string = ""
-        for car in car_list:
-            car_string = car.__str__()
-            string += car_string + "\n"
-        return string
-
     def get_list_of_cars(self, a_type, status):
-        #Heavy copyright Denni
+        # Heavy copyright Denni
         list = self.__car_repo.get_all_cars()
         car_list = []
         for car in list:
