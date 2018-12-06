731aeff4812fe8bf6211994088d7aefdba18489a
Jóhannes Ingi Torfason <johannest18@ru.is>
2018-12-06 15:01:30 +0000

    Færi car order úr admin í submenu

--- ui/sub_menus/car_menu.py
+++ ui/sub_menus/car_menu.py
@@ -12,6 +12,77 @@ class CarUI(object):
         self.__car_service = CarService()
         self.__uistandard = UIStandard(name)
 
+    def print_car_header(self, status_of_car):
+        self.print_header()
+        print("Bílayfirlit - {}".format(status_of_car))
+        print("\t{:<20} | {:<20} | {:<20}".format(
+            "Tegund", "Bílnúmer", "Staða"))
+        print("\t{}".format("-"*60))
+
+    def print_cars(self, status):
+        os.system('cls')
+        self.print_car_header(status)
+        # sækja upplýsingar til prentunar út frá "status" í parameter
+
+        print("\t{:<20} | {:<20} | {:<20}".format(
+            "Jeppi", "K1NG", "þriggjaDekkja"))
+        # prenta upplýsingar um bíl
+
+    def car_menu(self):
+        '''Bílayfirlit menu fyrir Kerfisstjóra'''
+        choice = ""
+        while choice not in HOMECOMMANDS:
+            os.system('cls')
+            choice = self.show_menu(
+                "Bílayfirlit\n\t1. Allir bílar\n\t2. Lausir bílar\n\t3. Í útleigu\n\t\
+4. Nýskrá bíl\n\t5. Afskrá bíl\n", "Veldu aðgerð: ")
+            if choice == "1":
+                self.print_cars("Allir bílar")
+            elif choice == "2":
+                self.print_cars("Lausir bílar")
+            elif choice == "3":
+                self.print_cars("Í útleigu")
+            elif choice == "4":
+                new_car = self.add_new_car()
+                self.__car_service.make_car(new_car)
+            elif choice == "5":
+                licence_plate = input("Númer bíls til afskráningar: ")
+                print()
+                approve_remove_car = input(
+                    "Viltu eyða bíl með bílnúmerið {} ((J)á/(N)ei): ".format(
+                        licence_plate))
+                if approve_remove_car.lower() == 'j':
+                    self.__car_service.remove_car(licence_plate)
+                    print("\nBíl með númerið {} hefur verið eytt!".format(
+                        licence_plate))
+                time.sleep(2)
+
+    def add_new_car(self):
+        os.system('cls')
+        approve_plate = ""
+        while approve_plate.lower() != "j":
+            os.system('cls')
+            self.print_header()
+            print("\tFlokkar\n\t{}".format("-"*10))
+            print("\t(J)eppi\n\t(F)ólksbíll\n\t(S)endibíll\n")
+            a_type = input("Flokkur: ")
+            if a_type.lower() == 'j':
+                a_type = "Jeppi"
+            elif a_type.lower() == 'f':
+                a_type = "Folksbill"
+            elif a_type.lower() == 's':
+                a_type = "Sendibill"
+            license_plate = input("Bílnúmer: ")
+            print()
+            approve_plate = input("Skrá {} með númerið {}\
+ ((J)á/(N)ei)? ".format(
+                a_type, license_plate))
+            new_car = Car(a_type, license_plate)
+        else:
+            print("\nBíll hefur verið skráður!")
+            time.sleep(2)
+            return new_car
+
     def boss_and_salesman_car_menu(self):
         """Pprentar bílayfirlits viðmót og tekur við input"""
         choice = ""
