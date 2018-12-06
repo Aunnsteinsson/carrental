73f1869381f3796c945061ea93ef30e1e11cbfe5
Aunnsteinsson <atliunnsteinsson@gmail.com>
2018-12-06 11:18:38 +0000

    return fyrir get employees

--- services/employeeservice.py
+++ services/employeeservice.py
@@ -8,16 +8,22 @@ class EmployeeService(object):
     def add_employee(self, employee):
         self.__employee_repo.add_employee(employee)
 
-    def get_employees(self, boss_or_admin=0):
-        # employee_list =
-        return self.__employee_repo.get_employees()
+    def get_employees(self, boss_or_admin=1):
+        employee_list = self.__employee_repo.get_employees()
+        for value in employee_list:
+            username = value[0]
+            password = value[1]
+            name = value[2]
+            position = value[3]
+            phone = value[4]
+            address = value[5]
 
-        # if boss_or_admin == 0:
-        #     pass
-        # else:
-        #     return"{:<10s}| {:<10s}| {:<10s}| {:<10s}|\
-        #      {:<10s}| {:<20s}".format(
-        #         username, password, name, position, phone, address)
+            if boss_or_admin == 0:
+                pass
+            else:
+                return"{:<10s}| {:<10s}| {:<10s}| {:<10s}|\
+                {:<10s}| {:<20s}".format(
+                    username, password, name, position, phone, address)
 
     def remove_employee(self, username):
         self.__employee_repo.remove_employee(username)
