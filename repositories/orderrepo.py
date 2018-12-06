185534a0e7a7e2ed6c3f1ac0d7d5217b8d6a3d66
logigeir <logi18@ru.is>
2018-12-06 02:35:06 +0000

    bætt við add_order og get_orders í orderrepo

--- repositories/orderrepo.py
+++ repositories/orderrepo.py
@@ -1,15 +1,22 @@
+from models.order import Order
+import csv
+
+
 class OrderRepo(object):
     def __init__(self):
         pass
 
-    def add_order(self):
+    def add_order(self, order):
+        with open("./data/orders.csv", "a+") as orders_file:
+            orders_file.write(order.__repr__() + "\n")
+
+    def remove_order(self, order_number):
         pass
 
-    def remove_order(self):
-        pass
-
-    def get_order(self):
-        pass
-
-    def __str__(self):
-        pass
+    def get_orders(self):
+        orders = []
+        with open("./data/orders.csv", "r") as orders_file:
+            csv_reader = csv.reader(orders_file)
+            for line in csv_reader:
+                orders.append(line)
+        return orders
