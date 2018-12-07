447cd2157451338b4d54f14dea6340b0a3c41dbc
theodorkri <theodorkri@ru.is>
2018-12-07 16:08:21 +0000

    Ýmsar litlar lagfæringar í textum

--- services/carservice.py
+++ services/carservice.py
@@ -4,28 +4,28 @@ from models.car import Car
 
 
 class CarService(object):
-    # Sér um aðgerðir með bíla
+    """ Sér um aðgerðir með bíla """
     def __init__(self):
         self.__car_repo = CarRepo()
 
     def make_car(self, car):
-        # Nýskráir bíl í kerfi
+        """ Nýskráir bíl í kerfi """
         self.__car_repo.add_car(car)
 
     def remove_car(self, licence_plate):
-        # Fjarlægir bíl úr kerfi
+        """ Fjarlægir bíl úr kerfi """
         self.__car_repo.remove_car(licence_plate)
 
     def change_status(self, licence_plate, new_status):
-        # Breytir stöðu bíls
+        """ Breytir stöðu bíls """
         self.__car_repo.change_status(licence_plate, new_status)
 
     def show_cars(self, licence_plate):
-        # Fall sem sýnir lista af bílum
+        """ Fall sem sýnir upplýsingar um bíl """
         self.__car_repo.get_car(licence_plate)
 
     def get_list_of_cars(self, a_type, status):
-        # Heavy copyright Denni
+        """ Fall sem sækir lista af öllum bílum """
         list = self.__car_repo.get_all_cars()
         car_list = []
         for car in list:
