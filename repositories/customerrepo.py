385ba35d9b23e54c051d296970abfbc397c8fc7b
Steingrímur Eyjólfsson <steingrimure@gmail.com>
2018-12-07 17:49:09 +0000

    Comments bætt við kóðann í customer repo

--- repositories/customerrepo.py
+++ repositories/customerrepo.py
@@ -8,29 +8,14 @@ KREDIT = 3
 
 class CustomerRepo(object):
     def __init__(self):
+        """Customer er hér sama og customer dict sem var notað til að
+        lesa gögn um viðskiptavini og færa þau í dictionary"""
         self.__customer = self.customer_dict()
 
-    def add_customer(self, new_customer):
-        ssn = new_customer.get__ssn()
-        self.__customer[ssn] = new_customer
-
-    def remove_customer(self, ssn):
-        for kennitala, value in self.__customer.items():
-            if kennitala == ssn:
-                del self.__customer[ssn]
-                return self.__customer
-        return False
-
-    def get_customer(self, ssn):
-        for kennitala, value in self.__customer.items():
-            if kennitala == ssn:
-                return self.__customer[ssn]
-        return False
-
-    def overview_customers(self):
-        return self.__customer
-
-    def customer_dict(self):
+     def customer_dict(self):
+        """Tekur við data úr customer.py og les það inn í dictionary
+        þar sem kennitalan er notuð sem key og hver customer er hluti af
+        customer klasanum er notað sem value"""
         customer_dict = {}
         with open("./data/customers.csv", "r") as customer_file:
             csv_reader = csv.reader(customer_file)
@@ -42,7 +27,38 @@ class CustomerRepo(object):
                     customer_dict[kennitala] = customer_class
         return customer_dict
 
+    def add_customer(self, new_customer):
+        """Tekur við kennitölu nýs viðskiptavinar og bætir honum við í 
+        customer dictionary"""
+        ssn = new_customer.get__ssn()
+        self.__customer[ssn] = new_customer
+
+    def remove_customer(self, ssn):
+        """Finnur key sem er kennitala viðskiptavinar, ef key passar við 
+        kennitöluna sem við starfsmaður leitar að þá eyðum við þeim
+        viðskiptavini út úr dictionary"""
+        for kennitala, value in self.__customer.items():
+            if kennitala == ssn:
+                del self.__customer[ssn]
+                return self.__customer
+        return False
+
+    def get_customer(self, ssn):
+        """Ef að kennitalan sem starfsmaður leitar að passar við
+        kennitölu í dictionary, þá skilum við þeim viðskiptavini. Annars
+        skilum við False"""
+        for kennitala, value in self.__customer.items():
+            if kennitala == ssn:
+                return self.__customer[ssn]
+        return False
+
+    def overview_customers(self):
+        """Skilar viðskiptavinayfirliti"""
+        return self.__customer
+
     def overwrite_customer_data(self):
+        """Þegar keyrslu er hætt, þá skrifum við inn allar upplýsingar aftur inn
+        i datafile sem heldur utan um viðskiptavini og þannig breytum við skránni"""
         with open("./data/customers.csv", "w", newline="") as customer_file:
             csv_writer = csv.writer(customer_file)
             for customer in self.__customer:
