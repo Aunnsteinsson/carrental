667cd42bb8bf9f5cd599a8282d5b0e438fa3148f
Steingrímur Eyjólfsson <steingrimure@gmail.com>
2018-12-10 17:37:07 +0000

    Fiktaði aðeins í order service klasanum

--- services/orderservice.py
+++ services/orderservice.py
@@ -12,20 +12,24 @@ class OrderService(object):
     def remove_order(self, order_number):
         self.__order_repo.remove_order(order_number)
 
-    def check_availability(self):
-        pass
+    def check_availability(self, start_date, finish_date, start_required_date, finish_required_date):
+        if start_required_date in range(start_date, finish_date) or finish_required_date in range(start_date, finish_date):
+            return False
+        else:
+            return True
 
     def change_time(self, order_number, new_time):
         order = self.__order_repo.get_orders(order_number)
         order.change_time(new_time)
 
-    def change_insurance(self):
-        pass
+    def change_insurance(self, order_number, new_insurance):
+        order = self.__order_repo.get_orders(order_number)
+        order.change_insurance(new_insurance)
 
     def show_orders(self):
         order_dict = self.__order_repo.get_orders()
         string_of_orders = ""
-        for kennitala, value in order_dict.items():
+        for order, value in order_dict.items():
             order_string = value.__str__()
             string_of_orders += order_string + "\n"
         return string_of_orders
