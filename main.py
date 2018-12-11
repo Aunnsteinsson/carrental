ddfb0426be678533e61679475ba73e0990098fbb
logigeir <logi18@ru.is>
2018-12-11 15:57:07 +0000

    main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from ui.BossUi import BossUI
 from ui.SalesmanUi import SalesmanUI
 
 user = LoginUI()
+try:
+    emp_type, username = user.main_menu()
+    if emp_type == "admin":
+        user = AdminUI(username, "Kerfisstjóri")
+    elif emp_type == "yfirmadur":
+        user = BossUI(username, "Yfirmaður")
+    elif emp_type == "soludeild":
+        user = SalesmanUI(username, "Söludeild")
 
-emp_type, username = user.main_menu()
-
-if emp_type == "admin":
-    user = AdminUI(username, "Kerfisstjóri")
-elif emp_type == "yfirmadur":
-    user = BossUI(username, "Yfirmaður")
-elif emp_type == "soludeild":
-    user = SalesmanUI(username, "Söludeild")
-
-user.main_menu()
+    user.main_menu()
+except TypeError:
+    print("Notandi ekki á skrá")
