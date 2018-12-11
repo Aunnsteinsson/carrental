c7c07052300d13ec8a684948fddd19b66f192e77
logigeir <logi18@ru.is>
2018-12-11 13:54:08 +0000

    Employee home aðgerðir lagaðar, villmeldingar

--- logimain.py
+++ logimain.py
@@ -1,7 +1,11 @@
 from ui.AdminUi import AdminUI
+# from ui.loginUI import LoginUI
 
 
 # logi
 k1 = AdminUI("logigeir")
 
 k1.main_menu()
+
+# a1 = LoginUI()
+# a1.main_menu()
--- ui/sub_menus/employee_menu.py
+++ ui/sub_menus/employee_menu.py
@@ -5,7 +5,7 @@ from services.employeeservice import EmployeeService
 from ui.ui_standard_functions import UIStandard
 # logi
 
-HOMECOMMANDS = ["h", "H", "s", "S"]
+HOMECOMMANDS = ["h", "s"]
 VALIDJOB = ["k", "s", "y"]
 
 
@@ -38,7 +38,7 @@ class EmployeeUI(object):
         os.system('cls')
         choice = ""
         get_pass = 1
-        while choice not in HOMECOMMANDS:
+        while choice.lower() not in HOMECOMMANDS:
             os.system('cls')
             self.__uistandard.print_header()
             self.print_employee_header()
@@ -56,57 +56,69 @@ class EmployeeUI(object):
             elif choice == "2":
                 # Breyta einhverju við employee
                 self.edit_employee()
+            else:
+                return choice
 
     def delete_employee(self, employees_list_string):
         print("\nEyða\n{}".format("-"*40))
         username = input("Notandanafn: ")
-        # athugar hvort notandi sé í kerfi
-        check = self.__employee_service.check_if_valid(username)
-
-        if check:
-            choice = input("Ertu viss? ((J)á/(N)ei): ")
-            if choice.lower() == "j":
-                self.__employee_service.remove_employee(username)
-                print("{}\nNotanda hefur verið eytt!\n".format("-"*40))
-            elif choice.lower() == "n":
-                print("{}\nHætt við aðgerð - Fer á upphafssíðu!\n".format(
-                    "-"*40))
-            time.sleep(2)
-            choice = "h"
-        else:
-            print("\nNotandanafn ekki á skrá.")
-            time.sleep(2)
+        if username.lower() not in HOMECOMMANDS:
+            # athugar hvort notandi sé í kerfi
+            check = self.__employee_service.check_if_valid(username)
+            if check:
+                choice = input("Ertu viss? ((J)á/(N)ei): ")
+                if choice.lower() == "j":
+                    self.__employee_service.remove_employee(username)
+                    print("{}\nNotanda hefur verið eytt!\n".format("-"*40))
+                    self.save_employees()
+                elif choice.lower() == "n":
+                    print("{}\nHætt við aðgerð!\n".format(
+                        "-"*40))
+                time.sleep(2)
+            else:
+                print("\nNotandanafn ekki á skrá.")
+                time.sleep(2)
+            return choice
 
     def edit_employee(self):
         '''Menu fyrir breytingu á starfsmanni'''
         print("\nBreyta\n{}".format("-"*40))
         username = input("Notandanafn: ")
-        # checka ef notandanafn er til
-        print("Hverju skal breyta?\n{}".format("-"*40))
-        print("\t1. Lykilorð\n\t2. Nafn\n\t3. Heimilisfang\n\t4. Sími\n")
-        choice = ""
-        new_value = ""
-        while choice not in HOMECOMMANDS:
-            choice = input("Veldu aðgerð: ")
-            if choice == "1":
-                new_value = input("Nýtt lykilorð: ")
-            elif choice == "2":
-                new_value = input("Nýtt nafn: ")
-            elif choice == "3":
-                new_value = input("Nýtt heimilisfang: ")
-            elif choice == "4":
-                new_value = input("Nýr sími: ")
+        if username.lower() not in HOMECOMMANDS:
+            # athugar hvort notandi sé í kerfi
+            check = self.__employee_service.check_if_valid(username)
+            if check:
+                print("Hverju skal breyta?\n{}".format("-"*40))
+                print("\t1. Lykilorð\n\t2. Nafn\n\t3. Heimilisfang\n\t4. Sími\n")
+                choice = ""
+                new_value = ""
+                while choice.lower() not in HOMECOMMANDS:
+                    choice = input("Veldu aðgerð: ")
+                    if choice == "1":
+                        new_value = input("Nýtt lykilorð: ")
+                    elif choice == "2":
+                        new_value = input("Nýtt nafn: ")
+                    elif choice == "3":
+                        new_value = input("Nýtt heimilisfang: ")
+                    elif choice == "4":
+                        new_value = input("Nýr sími: ")
 
-            # Skoða bug
-            self.__employee_service.change_employee(
-                username, choice, new_value)
-        choice = "h"
+                    # Skoða bug
+                    self.__employee_service.change_employee(
+                        username, choice, new_value)
+                    print("\nNotanda hefur verið breytt")
+                    self.save_employees()
+                    time.sleep(2)
+                    return choice
+            else:
+                print("\nNotandanafn ekki á skrá.")
+                time.sleep(2)
 
     def new_employee(self):
         '''Býr til nýjan starfsmann'''
         os.system('cls')
         choice = ""
-        while choice not in HOMECOMMANDS:
+        while choice.lower() not in HOMECOMMANDS:
             self.__uistandard.print_header()
             username = input("\tNotendanafn: ")
             password = input("\tLykilorð: ")
@@ -130,10 +142,11 @@ class EmployeeUI(object):
                     username, password, name, address, phonenumber, emp_type)
                 print("\n{}\nNýr notandi hefur verið skráður!\n".format(
                     "-"*40))
+                self.save_employees()
             else:
                 print("\nHætt við aðgerð - Fer á upphafssíðu!")
             time.sleep(2)
-            choice = "h"
+            return choice
 
     def show_employees(self):
         """ Prentar út alla starfsmenn í kerfi fyrir yfirmann"""
@@ -143,11 +156,9 @@ class EmployeeUI(object):
         employees_list_string = self.__employee_service.get_employees()
         print(employees_list_string)
         choice = ""
-        while choice not in HOMECOMMANDS:
+        while choice.lower() not in HOMECOMMANDS:
             choice = input("")
         return choice
 
     def save_employees(self):
-        save = input("Vista breytingar? ((J)á/(N)ei) ")
-        if save.lower() == 'j':
-            self.__employee_service.save_employees()
+        self.__employee_service.save_employees()
