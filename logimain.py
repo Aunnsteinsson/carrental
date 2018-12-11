9c289e0f9ff31012d8188f02f303a8325425fae5
Aunnsteinsson <atliunnsteinsson@gmail.com>
2018-12-11 18:58:39 +0000

    l í L í logimain

--- logimain.py
+++ logimain.py
@@ -1,9 +1,6 @@
 from ui.AdminUi import AdminUI
-<<<<<<< HEAD
-from ui.loginUI import LoginUI
-=======
+from ui.LoginUI import LoginUI
 # from ui.loginUI import LoginUI
->>>>>>> c7c07052300d13ec8a684948fddd19b66f192e77
 
 
 # logi
