f33b8d56be9c4e13fac7d460109c477459f4058f
Aunnsteinsson <atliunnsteinsson@gmail.com>
2018-12-13 14:13:42 +0000

    employee menu sett i miðju

--- ui/sub_menus/employee_menu.py
+++ ui/sub_menus/employee_menu.py
@@ -26,14 +26,9 @@ class EmployeeUI(object):
         Prentar haus fyrir starfmannayfirlit kerfisstjóra þar sem hann sér
         einnig lykilorð
         '''
-<<<<<<< HEAD
         print("{:<100}".format("\nStarfsmenn - Starfsmannayfirlit kerfisstjóra\n"))
         self.__uistandard.line_seperator()
         print("{:^10s}| {:^10s}| {:^25s}| {:^25s}| {:^10s}| {:^12s}".format(
-=======
-        self.__uistandard.print_location_header("Starfsmenn - Starfsmannayfirlit kerfisstjórans")
-        print("{:<10s}| {:<10s}| {:<25s}| {:<25s}| {:<10s}| {:<12s}".format(
->>>>>>> 841fd9622195e5c43ae8e58565804da01d85da30
             "Notandi", "Lykilorð", "Nafn", "Heimilisfang", "Sími", "Hlutverk"))
         self.__uistandard.line_seperator()
 
@@ -41,7 +36,8 @@ class EmployeeUI(object):
         '''
         Prentar haus fyrir starfmannayfirlit yfirmanns
         '''
-        self.__uistandard.print_location_header("Starfsmenn - Starfsmannayfirlit yfirmanns")
+        self.__uistandard.print_location_header(
+            "Starfsmenn - Starfsmannayfirlit yfirmanns")
         print("{:^25s}| {:^10s}| {:^25s}| {:^10s}| {:^12s}".format(
             "Nafn", "Notandi", "Heimilisfang", "Sími", "Hlutverk"))
         self.__uistandard.line_seperator()
