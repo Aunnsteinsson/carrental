72d959e88a1b6a4f3c07dd9e64ea71b1dd82807d
Steingrímur Eyjólfsson <steingrimure@gmail.com>
2018-12-14 14:43:55 +0000

    Comments í repo filea

--- repositories/customerrepo.py
+++ repositories/customerrepo.py
@@ -66,53 +66,6 @@ class CustomerRepo(object):
         with open("./data/customers.csv", "w", newline="", encoding="utf-8") as customer_file:
             csv_writer = csv.writer(customer_file)
             csv_writer.writerow(list_of_customers)
-            for ssn, customer in self.__customer.items():
+            for _, customer in self.__customer.items():
                 temp_list = customer.__repr__().split(",")
                 csv_writer.writerow(temp_list)
-
-
-#####################################GEYMSLA####################################################
-
-    """def get_indicators(list_of_employees):
-    Tekur við lista og flokkar eftir kennitölu, nafni, símanr og kreditkorti
-    kennitala = [lst[KENNITALA] for lst in list_of_employees]
-    nafn = [lst[NAFN] for lst in list_of_employees]
-    simi = [lst[SIMI] for lst in list_of_employees]
-    kreditkort = [lst[KREDIT] for lst in list_of_employees]
-    return kennitala, nafn, simi, kreditkort"""
-
-    """def add_customer(self, customer):
-            with open("./data/customers.csv", "a+") as customer_file:
-                ssn = customer.get__ssn()
-                name = customer.get_name()
-                phone_number = customer.get_phone_number()
-                creditcard_number = customer.get_creditcard_number()
-                customer_file.write("{},{},{},{}\n".format(
-                    ssn, name, phone_number, creditcard_number))"""
-
-    """def get_customer(self, ssn):
-            with open("./data/customers.csv", "r") as customer_file:
-                csv_reader = csv.reader(customer_file)
-                for row in csv_reader:
-                    if row:
-                        if row[0] == ssn:
-                            return row
-            return None"""
-
-    """def remove_customer(self, ssn):
-        with open("./data/customers.csv", "r") as customer_input:
-            with open("./data/customers_edit.csv", "w", newline="") as customer_output:
-                csv_reader = csv.reader(customer_input)
-                csv_writer = csv.writer(customer_output)
-                for row in csv_reader:
-                    if row:
-                        if row[0] != ssn:
-                            csv_writer.writerow(row)"""
-
-    """with open("./data/customers.csv", "w", newline="") as new_customer_file:
-            with open("./data/customers_edit.csv", "r") as new_customer_edit:
-                csv_reader = csv.reader(new_customer_edit)
-                csv_writer = csv.writer(new_customer_file)
-                for row in csv_reader:
-                    if row:
-                        csv_writer.writerow(row)"""
--- repositories/orderrepo.py
+++ repositories/orderrepo.py
@@ -8,28 +8,21 @@ CAR = 4
 PRICE = 5
 INSURANCE = 6
 DISCOUNT = 7
-# lgoi
 
 
 class OrderRepo(object):
     def __init__(self):
-        '''
-        Kallar í order_dict fallið og gefur self.__orders orðabókina.
-        '''
+        """Kallar í order_dict fallið og gefur self.__orders dictionary."""
         self.__orders = self.order_dict()
 
     def add_order(self, order_number, order):
-        '''
-        Bætir við pöntun í orðabók sem unnið er með.
-        '''
+        """Bætir við pöntun í dictionary sem unnið er með."""
         self.__orders[order_number] = order
 
     def remove_order(self, order_number):
-        '''
-        Notar order_number sem key til að leita í orða með pöntunum,
-        ef í dict, þá eyðir fallið þeirri pöntun.
-        '''
-        for ordernr, value in self.__orders.items():
+        """Notar order_number sem key til að leita í dictionary með pöntunum,
+        ef í dict, þá eyðir fallið þeirri pöntun."""
+        for ordernr, _ in self.__orders.items():
             if ordernr == order_number:
                 print(self.__orders[order_number])
                 del self.__orders[order_number]
@@ -37,34 +30,30 @@ class OrderRepo(object):
         return False
 
     def get_order(self, order_number):
+        """Tekur við order_number og skilar pöntuninni sem passar
+        við það númer"""
         order = self.__orders[order_number]
         return order
 
     def get_orders(self):
-        '''
-        Skilar orders orðabók til vinnslu.
-        '''
+        """Skilar orders dictionary til vinnslu."""
         return self.__orders
 
     def save_new_orders(self):
-        '''
-        Vistar upplýsingar úr orðabók í csv skrá sem heldur utan
-        um upplýsingarnar.
-        '''
+        """Vistar upplýsingar úr dictionary í csv skrá sem heldur utan
+        um upplýsingarnar."""
         orders_header = "order_number,duration,ssn,car_number,price,insurance,discount"
         with open("./data/orders.csv", "w", newline="", encoding="utf-8") as orders_file:
             csv_writer = csv.writer(orders_file)
             csv_writer.writerow(orders_header.split(','))
-            for order_number, info in self.__orders.items():
+            for _, info in self.__orders.items():
                 order_string = info.__repr__().split(",")
                 csv_writer.writerow(order_string)
 
     def order_dict(self):
-        '''
-        Tekur við gögnum, upplýsingum um pantanir, úr orders.csv og les inn í
-        orðabók. Þá er lykillinn pöntunarnúmerið og gildið er Orders
-        klasinn með upplýsingunum.
-        '''
+        """Tekur við gögnum, upplýsingum um pantanir, úr orders.csv og les inn í
+        dictionary. Þá er lykillinn pöntunarnúmerið og gildið er Orders
+        klasinn með upplýsingunum."""
         order_dict = {}
         with open("./data/orders.csv", "r", encoding="utf-8") as orders_file:
             csv_reader = csv.reader(orders_file)
